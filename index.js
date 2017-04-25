5ef005e467dea8b1a6b0edb96fed8e93946799fd
christopher-hague <christopher.a.hague@gmail.com>
2017-04-25 16:13:14 -0400

    done

--- index.js
+++ index.js
@@ -21,3 +21,30 @@ const zebraStripes = [
   { width: 0.59, color: null },
   { width: 7.31, color: null }
 ];
+
+var sortedRobots = robots.map(function(robot) {
+  if (knownDecepticons.includes(robot.name)) {
+    return Object.assign({}, robot, {
+      alliance: "decepticon",
+      isTransformed: true
+    })
+  } else {
+    return Object.assign({}, robot, {
+      alliance: "autobot",
+      isTransformed: true
+    })
+  }
+})
+
+
+var coloredZebraStripes = zebraStripes.map(function(cur, i) {
+  if (i % 2 == 0) {
+    return Object.assign({}, cur, {
+      color: "black"
+    })
+  } else {
+    return Object.assign({}, cur, {
+      color: "white"
+    })
+  }
+})
